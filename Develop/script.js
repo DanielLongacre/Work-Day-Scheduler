62c568962fb67dbf318eee9077d1191eeddc52f5
Daniel Longacre <danieljlongacre@gmail.com>
2023-07-30 14:12:45 -0400

    Able to console log items from storage and current hour

--- Develop/script.js
+++ Develop/script.js
@@ -26,7 +26,9 @@ $(function () {
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
-
+  var militaryHours = [9, 10, 11, 12, 13, 14, 15, 16, 17];
+  var currentHour = moment().format('h a');
+  console.log(currentHour);
 
 
 
@@ -35,8 +37,12 @@ $(function () {
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
+  var item = JSON.parse(localStorage.getItem('scheduleItem'));
+  for(var i = 0; i < item.length; i++) {
+    console.log(item[i].description);
+  }
   
-
+  
   
 
 
